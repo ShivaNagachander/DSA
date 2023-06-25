988cfa42dc2c0cdd4a0f6d29fdc938610ee65c5c
SHIVA NC <123239393+ShivaNagachander@users.noreply.github.com>
2023-06-25 17:59:37 +0530

    Create 9.7.quick_sort.c

---
+++ DSA-C/9.7.quick_sort.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quicksort(int arr[], int left, int right) {
+    if (left < right) {
+        int pivotIndex = partition(arr, left, right);
+
+        quicksort(arr, left, pivotIndex - 1);
+        quicksort(arr, pivotIndex + 1, right);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements: \n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Input Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    quicksort(arr, 0, n - 1);
+
+    printf("\nSorted Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
